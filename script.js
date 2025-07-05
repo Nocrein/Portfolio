96b3a8a147254fcb23d6c9729ed99b010537c388
Nocrein <dwynrex11@gmail.com>
2025-07-06 00:35:06 +0800

    Your commit message here

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btnProjects = document.getElementById('btn-projects');
+  const btnContact = document.getElementById('btn-contact');
+
+  btnProjects.addEventListener('click', () => {
+    location.href = '#projects';
+  });
+
+  btnContact.addEventListener('click', () => {
+    location.href = '#contact';
+  });
+});
